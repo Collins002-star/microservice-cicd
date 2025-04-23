455a5ddddd62c87f014c8fc63a0d3d15d25690b7
Collins002-star <collinscheruiyot002@gmail.com>
2025-04-23 09:54:55 +0300

    Initial commit: Microservice app with Docker and CI/CD

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.send('Hello from the Microservice!');
+});
+
+app.listen(PORT, () => {
+    console.log(`Microservice running on port ${PORT}`);
+});
